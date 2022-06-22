9019c39d66a2211537fa68b387375256884ec500
Ahmed Osman <ahmedosman36610@gmail.com>
2022-06-21 21:42:06 -0700

    worked on landing page

--- src/App.js
+++ src/App.js
@@ -4,37 +4,105 @@ import {
   Box,
   Text,
   Link,
+  Heading,
   VStack,
+  HStack,
   Code,
   Grid,
+  Image,
+  Stack,
+  StackDivider,
+  Icon,
+  Button,
+  Center,
+  useColorModeValue,
+  Flex,
   theme,
 } from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+import { ColorModeSwitcher } from './components/ColorModeSwitcher';
+import { FaBeer } from 'react-icons/fa';
+import { FcGoogle } from 'react-icons/fc';
+import { FaFacebook } from 'react-icons/fa';
+const Feature = ({ text, icon, iconBg }) => {
+  return (
+    <Stack direction={'row'} align={'center'}>
+      <Flex
+        w={8}
+        h={8}
+        align={'center'}
+        justify={'center'}
+        rounded={'full'}
+        bg={iconBg}
+      >
+        {icon}
+      </Flex>
+      <Text fontWeight={600}>{text}</Text>
+    </Stack>
+  );
+};
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
+      <HStack h="100%" w="100%">
+        <Image src="images/signinhero.png" w="50%"></Image>
+        <VStack>
+          <Stack spacing={4}>
+            <Heading>Trader Dashboard</Heading>
+            <Text color={'gray.800'} fontSize={'lg'}>
+              Join Now!
             </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
+            <Stack
+              spacing={4}
+              divider={
+                <StackDivider
+                  borderColor={useColorModeValue('gray.100', 'gray.700')}
+                />
+              }
             >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
-      </Box>
+              {/* Google */}
+              <Button w={'full'} variant={'outline'} leftIcon={<FcGoogle />}>
+                <Center>
+                  <Text>Sign up with Google</Text>
+                </Center>
+              </Button>
+              {/* Facebook */}
+              <Button
+                w={'full'}
+                colorScheme={'facebook'}
+                leftIcon={<FaFacebook />}
+              >
+                <Center>
+                  <Text>Sign Up with Facebook</Text>
+                </Center>
+              </Button>
+
+              <StackDivider>
+                <Center>Already have an account?</Center>
+              </StackDivider>
+              <Button
+                /* flex={1} */
+                px={4}
+                fontSize={'sm'}
+                rounded={'full'}
+                bg={'blue.400'}
+                color={'white'}
+                boxShadow={
+                  '0px 1px 25px -5px rgb(66 153 225 / 48%), 0 10px 10px -5px rgb(66 153 225 / 43%)'
+                }
+                _hover={{
+                  bg: 'blue.500',
+                }}
+                _focus={{
+                  bg: 'blue.500',
+                }}
+              >
+                Sign In
+              </Button>
+            </Stack>
+          </Stack>
+        </VStack>
+      </HStack>
     </ChakraProvider>
   );
 }
